000904521c6ba19f3f1472d74f2d246ddabbaf3a
Grayson Adkins <grayson@idyl.tech>
2020-12-23 16:10:46 -0600

    addins NODE_ENV block

--- server.js
+++ server.js
@@ -22,6 +22,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
 
+// Step 3
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+  });
+}
+
 app.listen(PORT, () => {
   log(`Server is starting at PORT: ${PORT}`);
 });
